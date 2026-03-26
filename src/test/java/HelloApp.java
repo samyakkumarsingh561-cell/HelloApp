a92a44cc5d10aefd9604122f8fee6f56f7f76eb9
samyak <samyakkumarsingh561@gmail.com>
2026-03-26 09:19:50 +0530

    uc6-display-name

--- src/test/java/HelloApp.java
+++ src/test/java/HelloApp.java
@@ -1,13 +1,14 @@
 /**
- * UC 5: Display "Hello" with Multiple Command-Line Arguments using Enhanced For Loop
- * or Default Message.
+ * HelloApp - UC6
+ * Display "Hello" with Multiple Command-Line Arguments using substring
+ * to Remove Trailing Delimiter.
  *
- * Usage: java HelloAppUC5 [name1] [name2] ... [nameN]
+ * Usage: java HelloAppUC6 [name1] [name2] ... [nameN]
  * - If names are provided, it will display "Hello, [Name1], [Name2], ...!".
  * - If no names are provided, it will display "Hello, World!".
  *
  * @author Developer Name
- * @version 5.0
+ * @version 6.0
  * @since UC1
  */
 
@@ -19,17 +20,14 @@ public class HelloApp {
             // Default greeting if no names are provided
             name = "World";
         } else {
-            // Build a comma-separated list of names using enhanced for loop
+            // Build a comma-separated list of names with trailing delimiter
             StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
             for (String arg : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(arg);
-                first = false;
+                nameBuilder.append(arg).append(", ");
             }
-            name = nameBuilder.toString();
+
+            // Remove the last ", " using substring
+            name = nameBuilder.substring(0, nameBuilder.length() - 2);
         }
 
         // Print the greeting
