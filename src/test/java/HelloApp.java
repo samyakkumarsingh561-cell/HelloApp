340e0c7f8e2857f376e01fecf436159065ed029e
samyak <samyakkumarsingh561@gmail.com>
2026-03-26 09:22:50 +0530

    uc7-display-name

--- src/test/java/HelloApp.java
+++ src/test/java/HelloApp.java
@@ -1,36 +1,30 @@
 /**
- * HelloApp - UC6
- * Display "Hello" with Multiple Command-Line Arguments using substring
- * to Remove Trailing Delimiter.
+ * HelloApp - UC7
+ * A simple Java application that greets multiple users by name
+ * using the String.join() method, or defaults to greeting "World".
  *
- * Usage: java HelloAppUC6 [name1] [name2] ... [nameN]
+ * Usage: java HelloAppUC7 [name1] [name2] ... [nameN]
  * - If names are provided, it will display "Hello, [Name1], [Name2], ...!".
  * - If no names are provided, it will display "Hello, World!".
  *
  * @author Developer Name
- * @version 6.0
+ * @version 7.0
  * @since UC1
  */
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name;
+        String names;
 
         if (args.length == 0) {
             // Default greeting if no names are provided
-            name = "World";
+            names = "World";
         } else {
-            // Build a comma-separated list of names with trailing delimiter
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String arg : args) {
-                nameBuilder.append(arg).append(", ");
-            }
-
-            // Remove the last ", " using substring
-            name = nameBuilder.substring(0, nameBuilder.length() - 2);
+            // Use String.join() to concatenate names with commas
+            names = String.join(", ", args);
         }
 
         // Print the greeting
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names + "!");
     }
 }
